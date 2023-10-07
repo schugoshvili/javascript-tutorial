07c6ae3e30fb9bbe083221652050a0f3ce2a40a0
schugoshvili <schugoshvili4@gmail.com>
2023-10-07 20:00:00 +0400

    some allerts
    javascript file allerts done

---
+++ javascript-sandbox/01-variable-data-types/01-console/script.js
@@ -0,0 +1,14 @@
+console.log(100);
+
+console.log('hello world');
+
+console.log(20, 'hello', true);
+
+
+
+const styles = 'padding: 10px; background-color: white; color: green';
+
+console.log('%cHello world', styles);
+
+
+

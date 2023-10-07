ff78bbe87b4dee383578bb90132d84a20fc8ba35
schugoshvili <schugoshvili4@gmail.com>
2023-10-07 20:16:20 +0400

    Update script.js
    shortcuts and comments

--- javascript-sandbox/01-variable-data-types/01-console/script.js
+++ javascript-sandbox/01-variable-data-types/01-console/script.js
@@ -1,3 +1,7 @@
+// this is a single line code
+
+
+
 console.log(100);
 
 console.log('hello world');
@@ -6,9 +10,10 @@ console.log(20, 'hello', true);
 
 
 
+
 const styles = 'padding: 10px; background-color: white; color: green';
+
 
 console.log('%cHello world', styles);
 
-
-
+console.log('I learnt Ctrl+~, I also learnt Ctrl+Shift+F, Ctrl+Shift+B, and many more, but the bold guy here says I wont remember them all eventually.');

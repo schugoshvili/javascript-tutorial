a0fd88b4b06b767f32c30b2af4b14fe75889fc66
schugoshvili <schugoshvili4@gmail.com>
2023-10-07 20:28:56 +0400

    added content

--- javascript-sandbox/01-variable-data-types/01-console/script.js
+++ javascript-sandbox/01-variable-data-types/01-console/script.js
@@ -16,4 +16,3 @@ const styles = 'padding: 10px; background-color: white; color: green';
 
 console.log('%cHello world', styles);
 
-console.log('I learnt Ctrl+~, I also learnt Ctrl+Shift+F, Ctrl+Shift+B, and many more, but the bold guy here says I wont remember them all eventually.');

3d004ed6747bb298fa297ada9f8de14bbcdf095d
schugoshvili <schugoshvili4@gmail.com>
2023-10-08 17:42:58 +0400

    reference types
    written in script.js file

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log('hello world');
+// string
+const firstName = 'sara';
+
+// number
+
+const age = 30;
+const temp = 98.9;
+
+// boolean
+
+const hasKids = true;
+
+// null
+
+const aptNumber = null;
+
+// undefined
+
+//let score;
+
+const score = undefined;
+
+// symbol
+
+const id = Symbol('id');
+
+// bigInt 
+
+const n = 907283762381401238n;
+
+// reference types
+
+const numbers = [1, 2, 3, 4];
+
+const person = {
+    name: 'John',
+};
+
+function sayHello() {
+    console.log('Hello');
+}
+
+const output = sayHello; 
+
+console.log(output); 
+console.log(output, typeof output); 

55b6730a8a8d4617daffffbb6534a0689be866b5
schugoshvili <schugoshvili4@gmail.com>
2023-10-08 20:31:37 +0400

    methods

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 let x;
 
-x= 5 + Number('5');
+const name = 'john';
+const age = 31;
 
-x = 5 * '5';
+x = 'Hello, my name is ' + name + 'and i am ' + age + ' years old';
 
-x = 5 + null;
+//Template literals
 
-x = Number(null);
-x = Number(true);
-x = Number(false);
-x = 5 + true;
-x = 5 + false;
-x = 5 + undefined;
+x = `Hello, my name is ${name} and i am ${age} years old`
+
+// string properties and methods
+
+const s = String('hello world');
+
+x = typeof s;
+
+x = s.length;
+
+// access value by key
+x = s[1];
+
+x = s.__proto__;
+
+x = s.toUpperCase();
+x = s.toLowerCase();
+x = s.charAt(1);
+x = s.indexOf('o');
+x = s.substring(1, 5);
+x = s.substring(7);
+x = s.slice(-11, -6);
+
+x = '                hello world';
+x = x.trim();
+x = s.replace('world', 'jon');
+
+x = s.includes('hello');
+console.log(x); // Log the result of the includes operation
+
+x = s.valueOf();
+x = s.split('');
 
 
-console.log(x, typeof x);
+console.log(s);

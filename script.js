337b0269e4c174505c4bba4a829a50fed103888c
schugoshvili <schugoshvili4@gmail.com>
2023-10-08 19:32:03 +0400

    arithmetics

--- script.js
+++ script.js
@@ -1,24 +1,64 @@
-let amount = 'hello';
+// 1. arithmetic operators
+
+let x;
+
+console.log(x);
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 7 % 5;
+
+// concatenation
+
+x = 'hello' + ' world';
+
+// exponent
+
+x = 2 ** 3;
+
+// increment
+
+x = 1;
+//x = x + 1;
+
+x++;
+// decrement
+
+//x = x - 1;
+
+x--;
+
+// assignment operators
+
+x = 10;
+
+x += 5;
+
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 5;
+x **= 5;
+
+// 3. comparison operators
+
+x = 2 == '2';
+
+x = 'samegrelo' == 'samegrelo';
+x = 2 === '2';
+
+x = 2 != 2;
+
+x = 2 != '2';
+
+x = 2 !== '2';
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
 
 
-// change string to number
-amount = parseInt(amount);
-//amount = +amount;
-//amount = Number(amount);
-
-// change number to string
-//amount = amount.toString();
-//amount = String(amount);
-
-//change string to decimal
-//amount = parseFloat(amount);
-
-//convert number to booleon
-//amount = Boolean(amount);
-
-console.log(Math.sqrt(-1));
-console.log(1 + NaN);
-console.log(undefined + undefined);
-console.log('foo' / 3);
-
-console.log(amount, typeof amount);
+console.log(x);

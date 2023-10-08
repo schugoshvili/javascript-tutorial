30cd0d0f7e97637182cdc2d5f70490609317d673
schugoshvili <schugoshvili4@gmail.com>
2023-10-08 20:47:27 +0400

    first challenge

--- script.js
+++ script.js
@@ -1,44 +1,15 @@
-let x;
+const myString = 'developer';
 
-const name = 'john';
-const age = 31;
+ // 'Developer'
 
-x = 'Hello, my name is ' + name + 'and i am ' + age + ' years old';
-
-//Template literals
-
-x = `Hello, my name is ${name} and i am ${age} years old`
-
-// string properties and methods
-
-const s = String('hello world');
-
-x = typeof s;
-
-x = s.length;
-
-// access value by key
-x = s[1];
-
-x = s.__proto__;
-
-x = s.toUpperCase();
-x = s.toLowerCase();
-x = s.charAt(1);
-x = s.indexOf('o');
-x = s.substring(1, 5);
-x = s.substring(7);
-x = s.slice(-11, -6);
-
-x = '                hello world';
-x = x.trim();
-x = s.replace('world', 'jon');
-
-x = s.includes('hello');
-console.log(x); // Log the result of the includes operation
-
-x = s.valueOf();
-x = s.split('');
+// how to capitalaze first letter using charAt() method, string [index], .toUppercase
+// substring() and slice() methods
 
 
-console.log(s);
+// Capitalize the first letter using charAt(), substring(), and toUpperCase()
+let myNewString = myString[0].toUpperCase() + myString.substring(1);
+
+
+myNewString = `${myString[0].toUpperCase()}${myString.slice(1)}`;
+
+console.log(myNewString);

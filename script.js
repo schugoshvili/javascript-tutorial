f898b30daee4860de4496fcb6f9a0f0922e8c396
schugoshvili <schugoshvili4@gmail.com>
2023-10-08 19:40:27 +0400

    coerce

--- script.js
+++ script.js
@@ -1,64 +1,17 @@
-// 1. arithmetic operators
-
 let x;
 
-console.log(x);
+x= 5 + Number('5');
 
-x = 5 + 5;
-x = 5 - 5;
-x = 5 * 5;
-x = 5 / 5;
-x = 7 % 5;
+x = 5 * '5';
 
-// concatenation
+x = 5 + null;
 
-x = 'hello' + ' world';
-
-// exponent
-
-x = 2 ** 3;
-
-// increment
-
-x = 1;
-//x = x + 1;
-
-x++;
-// decrement
-
-//x = x - 1;
-
-x--;
-
-// assignment operators
-
-x = 10;
-
-x += 5;
-
-x -= 5;
-x *= 5;
-x /= 5;
-x %= 5;
-x **= 5;
-
-// 3. comparison operators
-
-x = 2 == '2';
-
-x = 'samegrelo' == 'samegrelo';
-x = 2 === '2';
-
-x = 2 != 2;
-
-x = 2 != '2';
-
-x = 2 !== '2';
-
-x = 10 > 5;
-x = 10 < 5;
-x = 10 <= 5;
-x = 10 >= 5;
+x = Number(null);
+x = Number(true);
+x = Number(false);
+x = 5 + true;
+x = 5 + false;
+x = 5 + undefined;
 
 
-console.log(x);
+console.log(x, typeof x);

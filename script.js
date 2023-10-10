d124f7450a08b0e7430b5865c2782c9ed80b019a
schugoshvili <schugoshvili4@gmail.com>
2023-10-10 14:55:06 +0400

    object literals

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
-// array challenge
+// object literals 
 
-// use some of the array methods that we looked at to mutate the following
-// array to = the expected result bellow:
-
-const arr = [1, 2, 3, 4, 5];
-
-//expected console.log() result [6, 5, 4, 3, 2, 1, 0];
+let x;
 
 
+const person = {
+  name: 'john doe',
+  age: 30,
+  isAdmin: true,
+  address: {
+    street: '123 main st',
+    city: 'boston',
+    state: 'MA',
+  },
+  hobbies: ['music', 'sports'],
+};
 
-arr.push(6);
-arr.unshift(0);
+x = person.name;
+x = person['age'];
+x = person.address.state;
+x = person.hobbies[0];
 
-arr.reverse();
+person.name = 'jane doe';
+person['isAdmin'] = false;
+
+x = person;
+
+delete person.age;
+
+person.hasChildren = true;
+person.greet = function () {
+    console.log(`hello my name is ${this.name}`)
+}
+
+person.greet();
+
+const person2 = {
+    'first name': 'Brad',
+    'Last name': 'Traversy',
+}
+
+x = person2['first name'];
 
 
-
-console.log(arr);
-
-
-// challenge 2 
-
-//combine arr1 and arr2 into a new array called arr3 with the following elements
-
-//notice that both arr1 and arr2 include number 5. you will have to find a way 
-//to get rid ot the extra 5.
-
-//expected result: console.log(arr3); - [1,2,3,4,5,6,7,8,9,10]
-
-// hint concat method, spread operator as well as slice() splice() methods
-
-const arr1 = [1, 2, 3, 4, 5];
-const arr2 = [5, 6, 7, 8, 9, 10];
-
-
-const indexToRemove = 4;
-arr1.splice(indexToRemove, 1);
-
-const arr3 = arr1.concat(arr2);
-
-console.log(arr3);
+console.log(x);

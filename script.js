ccc3270ec600085689668ee7b625f91ba9916c71
schugoshvili <schugoshvili4@gmail.com>
2023-10-11 15:42:06 +0400

    nested scope

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
-//block scope
+// nester scope
 
-const x = 100;
-const foo = 1;
-var bar = 2;
+function first() {
+    const x = 100;
+
+    function second() {
+        const y = 200;
+        console.log(x + y);
+    }
+
+    second();
+}
+
+first();
 
 if (true) {
-    const y = 200
-    console.log(x + y);
+    const x = 100;
+    if (x === 100) {
+        const y = 200;
+        console.log(x + y);
+    }
+    
 }
-
-//console.log(x + y);
-
-for(var i = 0; i <= 10; i++) {
-console.log(i);
-}
-
-console.log(i);
-
-if (true) {
-    const a = 500;
-    let b = 600;
-    var c = 700;
-}
-
-
-console.log(c);
-
-
-function run()  {
-    var d = 100;
-    console.log(d);
-}
-
-run();
-
-console.log(d);

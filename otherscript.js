e9674d4f8faa1993c89e974531fa284e73221bf4
schugoshvili <schugoshvili4@gmail.com>
2023-10-11 16:28:19 +0400

    // immediately invoked function expression

---
+++ otherscript.js
@@ -0,0 +1,3 @@
+const user = 'brad';
+
+console.log(user);
--- script.js
+++ script.js
@@ -1,40 +1,19 @@
-// arrow functions
-
-//function add(a, b) {
-  //  return a + b;
-//}
-
-const add = (a, b) => { 
-    return a + b;
-};
-
-// implicit return
-
-const subtract = (a, b) => a - b;
-
-console.log(add(10, 2));
-console.log(subtract(10, 2));
-
-// can leave off {} with a single param
-
-const double = a => a * 2;
-
-// returning an object
-
-const createObj = () => ({
-    name: 'brad'
-});
-
-const numbers = [ 1, 2, 3, 4, 5];
-
-numbers.forEach(function (n) {
-    console.log(n);
-
-});
-// arrow function in a callback
-
-numbers.forEach(n => console.log(n));
+// immediately invoked function expression
 
 
-console.log(double(10));
-console.log(createObj());
+
+(function () {
+    const user = 'john';
+    console.log(user);
+    const hello = () => console.log('hello from the iffe');
+    hello();
+})();
+
+
+(function (name) {
+    console.log('hello ' + name);
+}) ('shawn');
+
+(function hello() {
+    console.log('hello');
+})();

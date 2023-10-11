096f7dd5de742e1e223db868713b5f927cde8cfb
schugoshvili <schugoshvili4@gmail.com>
2023-10-11 16:00:28 +0400

    arrow functions

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-// function declaration and XPathExpression
+// arrow functions
 
-console.log(addDollarSign(100));  // works
+//function add(a, b) {
+  //  return a + b;
+//}
 
-function  addDollarSign(value) {
-    return '$' + value;
-}
-
-
-
-
-console.log(addPlusSign(200)); // won't work
-
-const addPlusSign = function(value) {
-    return '+' + value;
+const add = (a, b) => { 
+    return a + b;
 };
 
+// implicit return
+
+const subtract = (a, b) => a - b;
+
+console.log(add(10, 2));
+console.log(subtract(10, 2));
+
+// can leave off {} with a single param
+
+const double = a => a * 2;
+
+// returning an object
+
+const createObj = () => ({
+    name: 'brad'
+});
+
+const numbers = [ 1, 2, 3, 4, 5];
+
+numbers.forEach(function (n) {
+    console.log(n);
+
+});
+// arrow function in a callback
+
+numbers.forEach(n => console.log(n));
+
+
+console.log(double(10));
+console.log(createObj());

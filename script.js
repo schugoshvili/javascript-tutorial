d23fac0a65458bb6531f2a761a698eec5d27a63c
schugoshvili <schugoshvili4@gmail.com>
2023-10-14 19:27:07 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,20 @@
-const age = 20;
+/* for (let i = 0; i <= 10; i++) {
 
-// using an if statement;
-
-if (age >= 18) {
-    console.log('you can vote');
-} else {
-    console.log('you cannot vote');
-}
-
-// using an ternary operator;
-
-age >= 18 ? console.log('you can vote') : console.log('you cannot vote');
-
-// assigning a conditional value tu a variable 
+    if (i === 6) {
+        console.log('6 is my favourite number');
+    } else {
+    console.log('number ' + i);
+    }
+} */ 
 
 
-const canVote = age >= 18 ? true : false;
-const canVote1 = age >= 18 ? 'you can vote' : ' u cant vote';
+/* let x = 'გამრავლების ტაბულა';
+console.log(x);
 
-console.log(canVote);
-console.log(canVote1);
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i}-ზე გამრავლების ტაბულა`);
 
-// multiple statements
-
-const auth = true;
-//let redirect;
-
-//if (auth) {
-  //  alert('welcome to the dashboard');
-    //redirect = '/dashboard'
-//} else {
-  //  alert ('access denied');
-    //redirect = '/login';
-//}
-
-// const redirect = auth ? (alert('welcome to the dashboard'), '/dashboard')
-// : (alert('access denied'), '/login');
-
-// console.log(redirect);
-
-// auth ? console.log('welcome to the dashboard') : null; 
-
-auth && console.log('welcome to the dashboard');
-
-
-
-
-
-
-
-
-
-
-
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
+} */

79c53110ce6269a98a7380e51bdbb275c31059e7
schugoshvili <schugoshvili4@gmail.com>
2023-10-14 20:09:05 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-/* for (let i = 0; i <= 10; i++) {
-
-    if (i === 6) {
-        console.log('6 is my favourite number');
-    } else {
-    console.log('number ' + i);
+/*for (let i = 0; i <= 20; i++) {
+    if (i === 15) {
+   console.log('breaking...');
+    break;
     }
-} */ 
-
-
-/* let x = 'გამრავლების ტაბულა';
-console.log(x);
-
-for (let i = 1; i <= 10; i++) {
-    console.log(`${i}-ზე გამრავლების ტაბულა`);
-
-    for (let j = 1; j <= 10; j++) {
-        console.log(`${i} * ${j} = ${i * j}`);
-    }
+    console.log(i);
 } */
+ 
+// continue 
+
+for (let i = 0; i <= 20; i++) {
+    if (i === 13) { console.log('skippin 13...');
+
+continue;
+}
+
+console.log(i);
+
+}

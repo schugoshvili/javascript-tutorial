e691cfc943c4442098ce13d4fd9c8494944d8b7d
schugoshvili <schugoshvili4@gmail.com>
2023-10-16 19:14:05 +0400

    new level

--- script.js
+++ script.js
@@ -1,63 +1,17 @@
-const people = [
-    { 
-        firstName: 'John', 
-        lastName: 'Doe',
-        email: 'john@example.com',
-        phone: '111-111-1111',
-        age: 30
-    }, 
-    {
-        firstName: 'Jane',
-        lastName: 'Poe',
-        email: 'poe@example.com',
-        phone: '111-111-1112',
-        age: 25,
-    },
-    {
-        firstName: 'bob',
-        lastName: 'foe',
-        email: 'foe@example.com',
-        phone: '111-111-1113',
-        age: 45,
-    },
-    {
-        firstName: 'sara',
-        lastName: 'soe',
-        email: 'soe@example.com',
-        phone: '111-111-1114',
-        age: 19,
-    },
-    {
-        firstName: 'jose',
-        lastName: 'koe',
-        email: 'koe@example.com',
-        phone: '111-111-1114',
-        age: 19,
-    }
-];
+//console.log(window);
 
-const youngPeople = people
-  .filter(person => person.age <= 25)
-  .map(person => ({ name: `${person.firstName} ${person.lastName}`, email: person.email }));
+//console.dir(window.document);
 
-console.log(youngPeople);
+//console.log(document.body.innerHTML);
 
+//console.log(document.links[0]);
 
-const numbers = [2, -30, 50, 20, -12, -9, 7];
+//document.body.innerHTML = '<h1>Hello World</h1>';
 
-const positiveSum = numbers 
-.filter((number) => number > 0)
-.reduce((acc, curr) => acc + curr, 0);
+//document.write('Hello from JS');
 
-console.log(positiveSum); // 79
+console.log(document.getElementById('main'));
+const main = document.getElementById('main');
 
-
-// challenge 3 
-
-const words = ['coder', 'programmer', 'developer' ];
-
-const cWords = words.map ((word) => {
-    return word[0].toUpperCase() + word.slice(1, word.length);
-});
-
-console.log(cWords);
+main.IinnerHTML = '<h1>Hello from mainc</h1>';
+document.querySelector('#main h1').innerText = 'hello';

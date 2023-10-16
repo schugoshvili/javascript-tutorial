51373f7d4dc74f0b1508b467696e7f0241b07cca
schugoshvili <schugoshvili4@gmail.com>
2023-10-16 17:34:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,133 +1,33 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const doubledNumbers = numbers.map((number) => 'number ' + number);
+const sum = numbers.reduce (function(accumulator, currentValue) {
+    return accumulator + currentValue;
+}, 0
+);
 
-console.log(doubledNumbers);
+const sum2 = numbers.reduce ((acc, cur) => acc + cur, 0);
 
-// same with foreach
+console.log(sum2);
 
-const doubledNumbers2 = [];
+// using a for loop
 
-
-numbers.forEach((number) => {
-  doubledNumbers2.push(number * 2);
-});
-
-console.log(doubledNumbers2);
-
-
-const companies = [
-    {
-      name: "Company One",
-      category: "Technology",
-      start: 2000,
-      end: 2010,
-    },
-    {
-      name: "Company Two",
-      category: "Retail",
-      start: 2005,
-      end: 2015,
-    },
-    {
-      name: "Company Three",
-      category: "Finance",
-      start: 2012,
-      end: 2018,
-    },
-    {
-      name: "Company Four",
-      category: "Healthcare",
-      start: 2007,
-      end: 2013,
-    },
-    {
-      name: "Company Five",
-      category: "Education",
-      start: 2011,
-      end: 2016,
-    },
-    {
-      name: "Company Six",
-      category: "Automotive",
-      start: 2002,
-      end: 2010,
-    },
-    {
-      name: "Company Seven",
-      category: "Entertainment",
-      start: 2014,
-      end: 2019,
-    },
-    {
-      name: "Company Eight",
-      category: "Hospitality",
-      start: 2008,
-      end: 2012,
-    },
-    {
-      name: "Company Nine",
-      category: "Real Estate",
-      start: 2010,
-      end: 2017,
-    },
-  ];
-
-  //create array of company names
-
-  const companyNames = companies.map((company) => company.name);
-
-  console.log(companyNames);
-
-  // create an array with just company and category 
-
-  const companyInfo = companies.map((company) => {
-    return {
-        name: company.name,
-        category: company.category,
-
+const sum3 = () => {
+    let acc = 0;
+    for (const cur of numbers) {
+        acc += cur;
     }
-  });
-
-  console.log(companyInfo);
-
-// create array of the length of each company in years  and names
-
-const companyYears = companies.map((company) => {
-    return {
-        name: company.name,
-        length: company.end - company.start + ' years'
-    }
-});
-
-console.log(companyYears);
-
-// chain map methods 
-
-const squareAndDouble = numbers
-
-.map((number) => Math.sqrt(number))
-.map((sqrt) => sqrt * 2);
-
-const squareAndDouble2 = numbers
-.map (function (number) {
-    return Math.sqrt(number);
-}
-)
-
-.map(function (sqrt) {
-    return sqrt * 2;
-})
-.map(function (sqrtDoubled) {
-    return sqrtDoubled * 3;
-  });
+    return acc;
+};
 
 
-  //chaining different methods
+const cart = [
+    {id: 1, name: 'product 1', price: 130},
+    {id: 2, name: 'product 2', price: 150},
+    {id: 3, name: 'product 3', price: 200},
+];
 
-  const evenDouble = numbers
-  .filter((number) => number % 2 === 0)
-  .map((number) => number * 2);
+const total = cart.reduce(function (acc, product) {
+    return acc + product.price
+}, 0);
 
-  console.log(evenDouble);
-
+console.log(total);

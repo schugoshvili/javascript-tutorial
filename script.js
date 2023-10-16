30b33ae0fac214f3d738d19540d83d56130bafea
schugoshvili <schugoshvili4@gmail.com>
2023-10-16 18:31:27 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,63 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-const sum = numbers.reduce (function(accumulator, currentValue) {
-    return accumulator + currentValue;
-}, 0
-);
-
-const sum2 = numbers.reduce ((acc, cur) => acc + cur, 0);
-
-console.log(sum2);
-
-// using a for loop
-
-const sum3 = () => {
-    let acc = 0;
-    for (const cur of numbers) {
-        acc += cur;
+const people = [
+    { 
+        firstName: 'John', 
+        lastName: 'Doe',
+        email: 'john@example.com',
+        phone: '111-111-1111',
+        age: 30
+    }, 
+    {
+        firstName: 'Jane',
+        lastName: 'Poe',
+        email: 'poe@example.com',
+        phone: '111-111-1112',
+        age: 25,
+    },
+    {
+        firstName: 'bob',
+        lastName: 'foe',
+        email: 'foe@example.com',
+        phone: '111-111-1113',
+        age: 45,
+    },
+    {
+        firstName: 'sara',
+        lastName: 'soe',
+        email: 'soe@example.com',
+        phone: '111-111-1114',
+        age: 19,
+    },
+    {
+        firstName: 'jose',
+        lastName: 'koe',
+        email: 'koe@example.com',
+        phone: '111-111-1114',
+        age: 19,
     }
-    return acc;
-};
-
-
-const cart = [
-    {id: 1, name: 'product 1', price: 130},
-    {id: 2, name: 'product 2', price: 150},
-    {id: 3, name: 'product 3', price: 200},
 ];
 
-const total = cart.reduce(function (acc, product) {
-    return acc + product.price
-}, 0);
+const youngPeople = people
+  .filter(person => person.age <= 25)
+  .map(person => ({ name: `${person.firstName} ${person.lastName}`, email: person.email }));
 
-console.log(total);
+console.log(youngPeople);
+
+
+const numbers = [2, -30, 50, 20, -12, -9, 7];
+
+const positiveSum = numbers 
+.filter((number) => number > 0)
+.reduce((acc, curr) => acc + curr, 0);
+
+console.log(positiveSum); // 79
+
+
+// challenge 3 
+
+const words = ['coder', 'programmer', 'developer' ];
+
+const cWords = words.map ((word) => {
+    return word[0].toUpperCase() + word.slice(1, word.length);
+});
+
+console.log(cWords);

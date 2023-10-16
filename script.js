cd3fb303d60987c68f3ed6d494f6788a40835e79
schugoshvili <schugoshvili4@gmail.com>
2023-10-16 16:46:16 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,95 @@
-const socials = ['twitter', 'facebook', 'LinkedIn', 'Instagram'];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ];
 
-//console.log(socials.__proto__);
+//const evenNumbers = numbers.filter(function (number) {
+  //  return number % 2 === 0;
+// }) ;
 
-//socials.forEach(function (item) {
-  //  console.log(item);
-// });
 
-//socials.forEach((item, index, arr) => console.log(`${index} - ${item}`, arr));
+//const evenNumbers = numbers.filter((number) => number % 2 === 0);
 
-function logSocials(social) {
-console.log(social);
-}
+/*const evenNumbers = [];
+numbers.forEach ((number) => {
+    if (number % 2 === 0) {
+        evenNumbers.push(number);
+    }
+});
 
-// socials.forEach(logSocials);
+console.log(evenNumbers); */
 
-const socialObjs = [
-    { name: 'twitter', url: 'http://twitter.com/' },
-    { name: 'facebook', url: 'http://facebook.com' },
-    { name: 'linkedin', url: 'http://linkedin.com' },
-    { name: 'instagram', url: 'http://instagram.com' }
+
+const companies = [
+    {
+      name: "Company One",
+      category: "Technology",
+      start: 2000,
+      end: 2010,
+    },
+    {
+      name: "Company Two",
+      category: "Retail",
+      start: 2005,
+      end: 2015,
+    },
+    {
+      name: "Company Three",
+      category: "Finance",
+      start: 2012,
+      end: 2018,
+    },
+    {
+      name: "Company Four",
+      category: "Healthcare",
+      start: 2007,
+      end: 2013,
+    },
+    {
+      name: "Company Five",
+      category: "Education",
+      start: 2011,
+      end: 2016,
+    },
+    {
+      name: "Company Six",
+      category: "Automotive",
+      start: 2002,
+      end: 2010,
+    },
+    {
+      name: "Company Seven",
+      category: "Entertainment",
+      start: 2014,
+      end: 2019,
+    },
+    {
+      name: "Company Eight",
+      category: "Hospitality",
+      start: 2008,
+      end: 2012,
+    },
+    {
+      name: "Company Nine",
+      category: "Real Estate",
+      start: 2010,
+      end: 2017,
+    },
   ];
-  
-  socialObjs.forEach((item) => console.log(item));
+
+// get only retail
+
+//const retailCompanies = companies.filter((company) => company.category === "Retail");
+
+// console.log(retailCompanies);
+
+// get companies in or after 2007 in or before 2012
+
+//const earlyCompanies = companies.filter((company) => company.start >= 2007
+//&& company.end <= 2012
+//)
+
+//console.log(earlyCompanies);
+
+// get companies that lasted 10 years or more
+
+const longCompanies = companies.filter((company) => company.end - company.start >= 10 );
+
+console.log(longCompanies);

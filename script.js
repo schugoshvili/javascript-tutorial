6786d842c6fa23cbcd0660a1064eb4d035c22be3
schugoshvili <schugoshvili4@gmail.com>
2023-10-23 17:22:30 +0400

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,38 @@ myArray.forEach(item => {
 });
 
 //Iterating through an Object's Properties:
+
+const myObject = { name: "John", age: 30, city: "New York" };
+
+for (const key in myObject) {
+    console.log(`${key}: ${myObject[key]}`);
+}
+
+//Iterating through an Object using Object.keys:
+
+const myObject = { name: "John", age: 30, city: "New York" };
+
+Object.keys(myObject).forEach(key => {
+    console.log(`${key}: ${myObject[key]}`);
+});
+
+// Iterating through an Array of Objects:
+const people = [
+    { name: "Alice", age: 25 },
+    { name: "Bob", age: 30 },
+    { name: "Charlie", age: 35 }
+];
+
+people.forEach(person => {
+    console.log(`${person.name} is ${person.age} years old.`);
+});
+
+
+//Iterating with map (returns a new array):
+
+javascript
+Copy code
+const myArray3 = [1, 2, 3, 4, 5];
+
+const doubledArray = myArray.map(item => item * 2);
+console.log(doubledArray);

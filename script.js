d01be6e2c7508a6b62e81fe90f4ec3acc58be2d1
schugoshvili <schugoshvili4@gmail.com>
2023-10-24 16:41:25 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const evenDouble = numbers
+// const products = ['apple', 'peach', 'watermelon', 'milk', 'chocolate'];
 
-.filter((number) => number % 2 === 0)
-.map((number) => number * 2);
 
-console.log(evenDouble);
+const sum = numbers.reduce (function (accumulator, currentValue) {
+return accumulator + currentValue;
 
+}, 0 );
+
+const sum1 = numbers.reduce((acc, cur) => acc + cur, 10);
+
+console.log(sum1);
+
+// using a for loop
+
+const sum3 = () => {
+    let acc = 0;
+    for (const cur of numbers) {
+        acc += cur;
+    }
+    return acc;
+}
+
+console.log(sum3());
+
+const cart = [
+    { id: 1, name: 'product 1', price: 130},
+    { id: 1, name: 'product 2', price: 150},
+    { id: 1, name: 'product 3', price: 175},
+    
+];
+
+const total = cart.reduce(function (acc, product){
+    return acc + product.price
+}, 0);
+
+console.log(total);

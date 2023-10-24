f8b11db1efb128122d566d2071675d11ab2ff430
schugoshvili <schugoshvili4@gmail.com>
2023-10-24 17:58:05 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,82 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const people = [
+    {
+      firstName: 'john',
+      lastName: 'doe',
+      email: 'john@.gmail.com',
+      phone: '111-111-1111',
+      age: 30,
+    },
+    {
+      firstName: 'jane',
+      lastName: 'smith',
+      email: 'jane@gmail.com',
+      phone: '222-222-2222',
+      age: 25,
+    },
+    {
+      firstName: 'alice',
+      lastName: 'jones',
+      email: 'alice@gmail.com',
+      phone: '333-333-3333',
+      age: 35,
+    },
+    {
+      firstName: 'bob',
+      lastName: 'wilson',
+      email: 'bob@gmail.com',
+      phone: '444-444-4444',
+      age: 28,
+    },
+    {
+      firstName: 'susan',
+      lastName: 'brown',
+      email: 'susan@gmail.com',
+      phone: '555-555-5555',
+      age: 40,
+    },
+    {
+      firstName: 'mike',
+      lastName: 'johnson',
+      email: 'mike@gmail.com',
+      phone: '666-666-6666',
+      age: 32,
+    },
+  ];
 
-// const products = ['apple', 'peach', 'watermelon', 'milk', 'chocolate'];
+//   people.forEach(person => {
+//     if (person.age < 34) {
+//       console.log(`name ${person.firstName} ${person.lastName}, ${person.email}`);
+//     }
+//   });
 
 
-const sum = numbers.reduce (function (accumulator, currentValue) {
-return accumulator + currentValue;
+const youngPeople = people
+.filter((person) => person.age <= 34)
+.map((person) => ({
+    name: person.firstName + ' ' + person.lastName,
+    email: person.email,
 
-}, 0 );
+}));
 
-const sum1 = numbers.reduce((acc, cur) => acc + cur, 10);
 
-console.log(sum1);
+console.log(youngPeople);
 
-// using a for loop
 
-const sum3 = () => {
-    let acc = 0;
-    for (const cur of numbers) {
-        acc += cur;
-    }
-    return acc;
-}
+const numbers = [2, -10, 58, 20, -12, -9, 7];
 
-console.log(sum3());
+const positiveNumbers = numbers
+.filter((numbers) => numbers > 0)
+.reduce((acc, cur) => acc + cur, 0);
 
-const cart = [
-    { id: 1, name: 'product 1', price: 130},
-    { id: 1, name: 'product 2', price: 150},
-    { id: 1, name: 'product 3', price: 175},
-    
-];
+console.log(positiveNumbers);
 
-const total = cart.reduce(function (acc, product){
-    return acc + product.price
-}, 0);
+const words = ['coder', 'programmer', 'developer'];
 
-console.log(total);
+// const capitalWords = words.map(word => {
+//   return word.charAt(0).toUpperCase() + word.slice(1);
+// });
+
+const capitalWords = words.map((word) => {
+    return word[0].toUpperCase(0) + word.slice(1, word.length);
+})
+console.log(capitalWords);

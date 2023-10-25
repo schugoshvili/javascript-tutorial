c5e05169672746ab3554c34d627a524178ee3018
schugoshvili <schugoshvili4@gmail.com>
2023-10-25 17:06:13 +0400

    readin book

---
+++ shopping-list-main/custom1.js
@@ -0,0 +1,138 @@
+// a = 21;
+// b = a * 2;
+// console.log(b);
+
+// age = prompt('please tell me your age');
+// console.log(age);
+
+// a = 2;
+// b = a + 1;
+
+// console.log(b);
+
+// var a = 20;
+// a += 1;
+// a *= 2;
+
+// console.log( a ); // 42
+
+
+// const a = { duration: 50 };
+
+// a.duration ??= 10;
+// console.log(a.duration);
+// // Expected output: 50
+
+// a.speed ??= 25;
+// console.log(a.speed);
+// // Expected output: 25
+
+// Define the phone price and employee responses
+// const phonePrice = 99.99;  // Define the phone price as a constant
+
+// function calculateCost(employeeResponse) {
+//     if (employeeResponse === "ninety-nine, ninety-nine") {
+//         return phonePrice;
+//     } else if (employeeResponse.toLowerCase() === "free") {
+//         return 0.00;
+//     }
+// }
+
+// // Ask the employee about the cost of a phone
+// const employeeResponse = "ninety-nine, ninety-nine"; // You can change this value
+
+// // Calculate the cost for two phones
+// const cost = calculateCost(employeeResponse);
+// const totalCost = cost * 2;
+
+// // Ask about the charger
+// const chargerIncluded = "yes"; // Change this to "no" if not included
+
+// // Print the results
+// console.log(`Cost of one phone: $${cost.toFixed(2)}`);
+// console.log(`Total cost for two phones: $${totalCost.toFixed(2)}`);
+// console.log(`Charger included: ${chargerIncluded}`);
+
+// var a = "42";
+// var b = Number( a );
+// console.log( a, typeof a ); // "42"
+// console.log( b, typeof b ); // 42
+
+// var TAX_RATE = 0.08; // 8% sales tax
+// var amount = 99.99;
+// amount = amount * 2;
+// amount = amount + (amount * TAX_RATE);
+// console.log( amount ); // 215.9784
+// console.log( amount.toFixed( 2 ) ); // "215.98"
+
+// const ACCESSORY_PRICE = 9.99;
+// var bank_balance = 302.13;
+// var amount = 99.99;
+// amount = amount * 2;
+// // can we afford the extra purchase?
+// if ( amount < bank_balance ) {
+//  console.log( "I'll take the accessory!" );
+//  amount = amount + ACCESSORY_PRICE;
+// }
+// // otherwise:
+// else {
+//  console.log( "No, thanks." );
+// }
+
+// let numOfCustomers = 10;
+
+// for (let i = 0; i <= 10; i++)
+
+// while (numOfCustomers > 0) {
+//     console.log( "How may I help you?" );
+//     // help the customer...
+//     numOfCustomers = numOfCustomers - 1;
+//    }
+//    // versus:
+//    do {
+//     console.log( "How may I help you?" );
+//     // help the customer...
+//     numOfCustomers = numOfCustomers - 1;
+//    } while (numOfCustomers > 0);
+   
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log("How may I help you?");
+//     // help the customer...
+// }
+
+// Reset numOfCustomers for the following loops
+// numOfCustomers = 10;
+
+// // Using a while loop
+// while (numOfCustomers > 0) {
+//     console.log("How may I help you?");
+//     // help the customer...
+//     numOfCustomers = numOfCustomers - 1;
+// }
+
+// Reset numOfCustomers for the next loop
+// numOfCustomers = 10;
+
+// // Using a do-while loop
+// do {
+//     console.log("How may I help you?");
+//     // help the customer...
+//     numOfCustomers = numOfCustomers - 1;
+// } while (numOfCustomers > 0);
+
+var i = 0;
+// a `while..true` loop would run forever, right?
+while (true) {
+ // keep the loop going?
+ if (i <= 9) {
+ console.log( i );
+ i++;
+
+ }
+ // time to stop the loop!
+ else {
+ break;
+ }
+}
+// 0 1 2 3 4 5 6 7 8 9

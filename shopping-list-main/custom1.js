369a723bc665b239e676f543e3b1858595bea34a
schugoshvili <schugoshvili4@gmail.com>
2023-11-03 17:13:59 +0400

    x

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,2 +1,19 @@
-var a = 42;
-var b = (a > 41) ? "hello" : "world";
+let x;
+
+x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x = Math.round(4.2);
+
+x = Math.ceil(4.2);
+
+x = Math.floor(5.9);
+
+x = Math.pow(2, 3);
+
+x = Math.min(4, 5, 3);
+
+x = Math.round(Math.random() * 10 + 1);
+
+console.log(x);

aef50a4806ff93d7b5b17286bc0df20508679533
schugoshvili <schugoshvili4@gmail.com>
2023-11-04 20:01:12 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,22 +1,24 @@
-let x;
+const numbers = [12, 45, 33, 29, 39];
 
-let d = new Date();
+console.log(numbers);
 
-x = d.toString();
-x = d.valueOf();
-x = d.getFullYear();
-x = d.getMonth() + 1;
-x = d.getDate();
-x = d.getDay();
-x = d.getHours();
-x = d.getMinutes();
-x = d.getSeconds();
-x = d.getMilliseconds();
-x = `${d.getFullYear()} - ${d.getMonth() + 1} - ${d.getDate()}`;
-x = x = x.split('').reverse().join('');
-x = Intl.DateTimeFormat('en-UK').format(d);
-x = Intl.DateTimeFormat('default', { month: 'long'}).format(d);
-x = d.toLocaleDateString('default', {month: 'short'});
+// array constructor
 
+const fruits = new Array('apple', 'grape', 'orange');
+
+x = `my favourite fruit is an ${fruits[2]}`;
+
+x = numbers.length;
+
+fruits[2] = 'grape';
+
+x = fruits
+
+// fruits.length = 2;
+
+fruits[3] = 'strawberry';
+
+fruits[fruits.length] = 'blueberry';
+fruits[fruits.length] = 'peach';
 
 console.log(x);

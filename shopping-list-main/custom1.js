dd0e2e262a3e66a5cbc17ca72e682e48692f9687
schugoshvili <schugoshvili4@gmail.com>
2023-11-05 15:52:29 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,49 +1,31 @@
-// const todo = {};
-const todo = new Object();
-
-todo.id = 1;
-todo.name = 'buy milk';
-todo.completed = false;
-
-x = todo;
+const firstName = 'john';
+const lastName = 'doe';
+const age = 30;
 
 const person = {
-    adress: {
-        coords: {
-            lat: 42.222,
-            lng: -11.287287        },
-    },
-};
-
-const obj1 = {a: 1, b: 2};
-const obj2 = {c: 3, d: 4};
-
-const obj3 = {...obj1, ...obj2};
-const obj4 = Object.assign({}, obj1, obj2);
-
-const todos = [{
-    id: 1,
-    name: 'buy milk'
-},
-{
-    id: 2,
-    name: 'make bed'
-},
-{
-    id: 1,
-    name: 'drink beer'
+    firstName,
+    lastName,
+    age
 }
-];
 
-x = todos[1];
+console.log(person.lastName);
 
-x = Object.keys(todo);
-x = Object.keys(todo).length;
+// destructure
 
-x = Object.values(todo);
+const todo = {
+    id: 1,
+    title: 'make bed',
+    user: {
+        name: 'John',
+    }
+}
 
-x = Object.entries(todo);
+const { id: todoId, title, user: {name}, } = todo;
 
-x = todo.hasOwnProperty('age');
+console.log(todoId);
 
-console.log(x);
+const numbers = [23, 67, 33, 49];
+
+const [...rest] = numbers;
+
+console.log(rest);

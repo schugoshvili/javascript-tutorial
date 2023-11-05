1440c35de01195689433be70b766865099997c47
schugoshvili <schugoshvili4@gmail.com>
2023-11-05 16:28:58 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,46 +1,7 @@
-const library = [{
-    title: 'crime and punishment',
-    author: 'dostoyevsky',
-    status: {
-        own: true,
-        reading: false,
-        read: false
-    }
-}, {
-    title: 'tarzanai',
-    author: 'devdariani',
-    status: {
-        own: true,
-        reading: false,
-        read: false
-    }
-}, {
-    title: 'xevisberi socha',
-    author: 'chavchuqadze',
-    status: {
-        own: true,
-        reading: false,
-        read: false
-    }
+function sayHello() {
+    console.log(
+        'hello world'
+    )
 }
-];
 
-// step 2
-
-library[0].status.read = true;
-library[1].status.read = true;
-library[2].status.read = true;
-
-// console.log(library);
-
-// step 3;
-
-const { title: firstBook } = library[0];
-
-console.log(firstBook);
-
-// json
-
-const json1 = JSON.stringify(library);
-
-console.log(json1);
+sayHello();

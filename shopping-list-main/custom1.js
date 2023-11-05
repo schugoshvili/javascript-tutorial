999422351172d016227c3ca0ecc42cfd5d7ba64d
schugoshvili <schugoshvili4@gmail.com>
2023-11-05 14:41:32 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,29 +1,41 @@
-// challenge 1 
+let x;
 
-// const arr = [1, 2, 3, 4, 5];
+const person = {
+    name: 'John Doe',
+    age: 30,
+    isAdmin: true,
+    address: {
+        street: '123 main st',
+        city: 'boston',
+        state: 'kakheti'
+    },
+    hobbies: [
+        'music', 'sports'
+    ]
+};
 
-// arr.push(6);
-// arr.unshift(0);
-// arr.reverse();
+x = person.name;
+x = person['age'];
+x = person.address.city
+x = person.hobbies[0];
+
+person.name = 'jane doe';
+person['isAdmin'] = false;
+
+delete person.age;
+
+person.hasChildren = true;
+
+person.greet = function () {
+    console.log(`hey my name is ${this.name}`);
+}
+
+x = person;
+
+person.greet();
 
 
-// console.log(arr);
 
 
-// challenge 2 
 
-
-const arr1 = [1, 2, 3, 4, 5];
-const arr2 = [5, 6, 7, 8, 9, 10];
-
-
-const x = arr1.concat(arr2)
-
-const arr3 = x;
-
-let y;
-
-y = arr3.splice(4, 1);
-
-
-console.log(arr3);
+console.log(x);

4dc438bc03c201eed165fd8c8f15d539123c8c22
schugoshvili <schugoshvili4@gmail.com>
2023-11-08 15:55:56 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,7 +1,26 @@
-function sayHello() {
-    console.log(
-        'hello world'
-    )
-}
+let x; 
 
-sayHello();
+ x = Math.sqrt(9);
+
+ x = Math.abs(-5);
+
+ x = Math.round(4.6);
+
+ x = Math.ceil(4.2);
+
+ x = Math.floor(4.8);
+
+ x = Math.pow(2, 4);
+
+ x = Math.min(4, 5);
+
+ x = Math.max(4, 5, 3);
+
+ x = Math.random();
+
+ x = Math.floor(Math.random() * 10 + 1);
+
+
+ console.log(x);
+
+

0c6789ebae7afe18e897c878954fc282ee86cba4
schugoshvili <schugoshvili4@gmail.com>
2023-11-11 20:49:54 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,9 +1,30 @@
-function add() {
-    const y = 50;
-    console.log(y);
+const x = 100;
+
+if (true) {
+    const y = 200;
+    console.log(x + y);
+};
+
+// clg(x + y) = error
+
+for (let i = 0; i <= 10; i++) {
+console.log(i);
 }
 
+// console.log(i);
 
-add();
+if (true) {
+    const a = 500;
+    let b = 600;
+    var c = 700;
+}
 
-console.log(y);
+function run() {
+    var d = 100;
+
+    console.log(d);
+}
+
+run();
+
+console.log(d);

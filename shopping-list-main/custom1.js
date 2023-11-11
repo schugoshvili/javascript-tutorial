7766359f0dfbced5aa3bf6cfc8acfa95bf07ce78
schugoshvili <schugoshvili4@gmail.com>
2023-11-11 23:16:12 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,17 +1,28 @@
+// challenge 1
 
+// const F2C = function(FH) {
+//     return (FH - 32) * 5/9;
+// };
 
-(function () {
-    const user = 'John';
-    console.log(user);
-    const hello = () => console.log('hello from the iffe');
-    hello();
-})();
+const F2C = FH => (FH - 32) * 5/9;
 
+console.log(F2C(0) + '\xB0');
 
-(function (name) {
-    console.log('hello ' + name);
-})('mendes');
+// challenge 2
 
-(function hello() {
-    console.log('hello');
-})();
+const numbers = [1, 2, 3, 4, 5];
+
+const minMax = () => ({
+    min: Math.min(...numbers),
+    max: Math.max(...numbers)
+});
+
+console.log(minMax());
+
+// challenge 3
+
+(function(width, length) {
+    console.log(`The area of a rectangle with width of ${width} 
+    and length of ${length} is ${width * length}`);
+})(5, 10);
+

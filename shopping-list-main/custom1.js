964a83323ab753a14143a2a3abb5c789f2c2dbea
schugoshvili <schugoshvili4@gmail.com>
2023-11-12 16:05:25 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,14 +1,30 @@
-function addDollarSign(value) {
-    return '$' + value;
-};
-
-console.log(addDollarSign(100)); 
-
-// 
+// function add(a, b) {
+//     return a + b;
+// }
 
 
-const addPlusSign = function (value) {
-    return '+' + value;
-};
 
-console.log(addPlusSign(100));
+const add = (a, b) => {
+    return a + b;
+ };
+
+
+ // implicit return
+ const subtract = (a, b) => a - b;
+
+ const double = (a) => a * 2;
+
+// returning an object
+
+const createObj = () => ({
+    name: 'brad',
+});
+
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach((n) => console.log(n));
+
+
+console.log(subtract(1, 2));
+console.log(createObj());
+console.log(double(20));

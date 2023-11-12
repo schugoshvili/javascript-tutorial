4b99dd0f8712ea0f3f7a6db8a9095d0012081cf6
schugoshvili <schugoshvili4@gmail.com>
2023-11-12 14:58:56 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,54 +1,23 @@
-function registerUser(user = 'bot', x = 'y') {
-    if (!user) {
-        user = 'bot';
+// function first() {
+//     const x = 100;
+
+//     function second() {
+//     const y = 200;
+//     console.log(x + y);
+// };
+// second();
+
+// }
+
+// first();
+
+if (true) {
+    const x = 100;
+
+    if (x === 100) {
+        const y = 200;
+        console.log(x + y);
     }
-    return user + ' is registered';
+
+    console.log(y);
 }
-
-console.log(registerUser('haha'));
-
-// rest params
-
-function sum(...numbers) {
-    let total = 0;
-
-    for (const num of numbers) {
-        total += num;
-    }
-return total;
-}
-
-
-console.log(sum(1, 2, 3));
-
-// Objects as params 
-
-function loginUser(user) {
-    return `The user ${user.name} with the id of ${user.id} is logged in 
-    as ${user.nickname};` };
-
-    const user = {
-        id: '#2323',
-        name: 'davit giorgobiani',
-        nickname: 'klezeshamamafrindiwertiliji'
-    }
-    console.log(loginUser(user));
-    console.log(loginUser({
-        id: 23,
-        name: 'sara',
-        nickname: 'alouluv'
-    }));
-
-    // arrays as params
-
-    function getRandom(...arr) {
-        const randomIndex = Math.floor(Math.random() * arr.length);
-
-        const item = arr[randomIndex];
-
-        console.log(item);
-    }
-
-    getRandom([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]);
-
-

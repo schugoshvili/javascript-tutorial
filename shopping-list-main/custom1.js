be34bf0f8994f0eea1d9af2d83aba71936b2e6f5
schugoshvili <schugoshvili4@gmail.com>
2023-11-12 16:09:29 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,30 +1,17 @@
-// function add(a, b) {
-//     return a + b;
-// }
+(function() {
+    const user = 'jonh';
+    console.log(user);
+    const hello = () => console.log('hello world');
+    hello();
+}
+)();
 
 
-
-const add = (a, b) => {
-    return a + b;
- };
-
-
- // implicit return
- const subtract = (a, b) => a - b;
-
- const double = (a) => a * 2;
-
-// returning an object
-
-const createObj = () => ({
-    name: 'brad',
-});
-
-const numbers = [1, 2, 3, 4, 5];
-
-numbers.forEach((n) => console.log(n));
+(function (name) {
+    console.log('hello ' + name + '!');
+})('shawn');
 
 
-console.log(subtract(1, 2));
-console.log(createObj());
-console.log(double(20));
+(function hello() {
+    console.log('hello');
+})();

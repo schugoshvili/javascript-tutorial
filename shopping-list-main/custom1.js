4a021fc56dc33eeff4fd014afc3987df061ce64c
schugoshvili <schugoshvili4@gmail.com>
2023-11-12 16:30:19 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,17 +1,28 @@
-(function() {
-    const user = 'jonh';
-    console.log(user);
-    const hello = () => console.log('hello world');
-    hello();
-}
-)();
+// challenge 1 
 
+function getCelsius(fheit) {
+    return (fheit - 32) * 5 / 9;
+};
 
-(function (name) {
-    console.log('hello ' + name + '!');
-})('shawn');
+console.log(`the temperature is ${getCelsius(32)}\xB0C`);
 
+// challenge 2 
 
-(function hello() {
-    console.log('hello');
-})();
+function minMax(arr) {
+    const min = Math.max(...arr);
+    const max = Math.min(...arr);
+    return {min, max};
+};
+
+console.log(minMax([1, 2, 3, 0, 4, 8, 5, 6]));
+
+// challenge 3 
+
+((length, width) => { 
+    const area = length * width;
+
+    const result = `the area of a rectangle with length of ${length}
+     and a width of ${width} is ${area}`;
+    alert(result); // Add this line to log the result
+})(2, 4);
+

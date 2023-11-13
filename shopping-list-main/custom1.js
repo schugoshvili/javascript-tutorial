1d69f1718aee3509c33b8ab9ad17680cdd00154d
schugoshvili <schugoshvili4@gmail.com>
2023-11-13 22:23:21 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,32 +1,16 @@
-const d = new Date (2022, 3, 10, 23, 0, 0);
-
-hour = d.getHours();
-const month = d.getMonth();
-
-switch (month) {
-    case 1: 
-    console.log('it is january');
-    break;
-    case 2: 
-    console.log('it is february');
-    break;
-    case 3: 
-    console.log('it is march');
-    break;
-    default:
-        console.log('it is neither of first three months');
+function calculator(x, y, operator) {
+    if ( operator === '-' ) {
+        return (x - y);
+    } else if ( operator === '+' ) {
+        return ( x + y);
+    } else if ( operator === '*' ) {
+        return ( x * y);
+    } else if ( operator === '/' ) {
+        return (x / y);
+    } else {
+        return ('error 404 not found');
+    }
 
 }
 
-
-switch (true) {
-    case hour < 12:
-    console.log('good morning');
-    break;
-    case hour < 16:
-        console.log('good afternoon');
-        break;
-    
-    default: console.log(' good night');
-
-}
+console.log(calculator(10, 5, '-'));

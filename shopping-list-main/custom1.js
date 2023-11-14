dcfacd6495dff70155b340e8b488499888f3fc02
schugoshvili <schugoshvili4@gmail.com>
2023-11-14 19:45:50 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,31 +1,18 @@
-console.log(10 > 20 && 3 > 15);
-console.log(10 > 20 || 30 > 15);
+const x = 10;
 
-// && will return first falsy value or the last value;
-let a;
-a = 10 && 20;
-a = 10 && 20 && 0 && 40;
+const y = 20;
 
-console.log(a);
+if (x > y) {
+    console.log(`${x} is greater than ${y}`);
+} else {
+    console.log(`${x} is not greater than ${y}`);
+}
 
-const posts = [];
-
-posts.length > 0 && console.log(posts[0]);
-
-// || will return the first truthy value or the last value
-
-let b;
-
-b = 10 || 400;
-b = '' || undefined;
+if (x !== y) {
+    const z = 20;
 
 
-console.log(b);
-
-// ?? - returns the right side operand when the left ins null or undefined
-
-let c;
-
-c = 12 ?? 40 ?? 80;
-
-console.log(c);
+    console.log('yes');
+} else {
+    console.log('no');
+}

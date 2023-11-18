a5ec677cef57aaa4f7df7e6f99b6e28c10b19304
schugoshvili <schugoshvili4@gmail.com>
2023-11-18 13:32:00 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,18 +1,14 @@
-const colourObj = {
-    colour1: 'Red',
-    colour2: 'blue',
-    colour3: 'green',
-    colour4: 'black',
-    colour5: 'rose',
-};
-
-for (const key in colourObj) {
-    console.log(key, colourObj[key], typeof colourObj[key], typeof key);
+function checkEvenOrOdd(number) {
+    if (number % 2 === 0) {
+        return 'even';
+    } else {
+        return 'odd';
+    }
 }
 
+// Example usage:
+var num1 = 8;
+console.log(num1 + " is " + checkEvenOrOdd(num1));
 
-const colourArr = ['red', 'green', 'blue', 'yellow'];
-
-for (const key in colourArr) {
-    console.log(colourArr[key]);
-}
+var num2 = 15;
+console.log(num2 + " is " + checkEvenOrOdd(num2));

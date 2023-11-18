8165e2af1eaecb47dd63d5660b9c4fae07e2ffdf
schugoshvili <schugoshvili4@gmail.com>
2023-11-18 14:51:35 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,34 +1,16 @@
-const items = ['book', 'table', 'chair', 'kite'];
+// const colourObj = {
+//     colour1: 'blue',
+//     colour2: 'orange',
+//     colour3: 'green',
+//     colour4: 'yellow'
+// };
 
-// for (let i = 0; i < items.length; i++) {
-//     console.log(items[i]);
+// for (const key in colourObj) {
+//     console.log(colourObj[key]);
 // }
 
-// const users = [
-// {name: 'brad'},
-// {name: 'dato'},
-// {name: 'ia'},];
+const colourArr = ['red', 'green', 'yellow'];
 
-// for (const item of items) {
-//     console.log(item);
-// }
-
-
-// for (const user of users) {
-//     console.log(user.name);
-// }
-
-// const str = 'hey sucer';
-
-// for (const letter of str) {
-//     console.log(letter);
-// }
-
-// const map = new Map();
-
-// map.set('name', 'John');
-// map.set('age', 3);
-
-// for (const [key, value] of map) {
-//     console.log(key, value);
-// }
+for (const colour in colourArr) {
+    console.log(colourArr[colour]);
+}

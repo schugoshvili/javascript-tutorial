3936a72d957614f1d612bbed5c10a972c45ea2f3
schugoshvili <schugoshvili4@gmail.com>
2023-11-22 20:52:43 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,32 +1,35 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// const sum = numbers.reduce(function (accumulator, currentValue) {
-//     return accumulator + currentValue;
-
-// }, 0);
-
-const sum2 = numbers.reduce((acc, curr) => acc + curr, 0);
-
-console.log(sum2);
-
-// using a forLoop 
-
-const sum3 = () => {
-    let acc = 0;
-    for (const cur of numbers) {
-        acc += cur;
-    }
-    return acc;
-};
-
-console.log(sum3());
-
-const cart = [
-    {id: 1, name: 'prod 1', price: 120},
-    {id: 2, name: 'prod 2', price: 556},
-    {id: 3, name: 'prod 3', price: 223},
+const people = [
+    {firstName: 'John', lastName: 'doe', email: 'john@example.com',
+phone: '123 123 123', age: 30},
+  {firstName: 'kevin', lastName: 'durant', email: 'kevin@example.com',
+phone: '22 22 12223', age: 25},
+{firstName: 'novak', lastName: 'djokovic', email: 'ndjk@example.com',
+phone: '33 33 123', age: 45},
+{firstName: 'rafael', lastName: 'nadal', email: 'rndl@example.com',
+phone: '44 123 123', age: 40},
+{firstName: 'roger', lastName: 'federa', email: 'rf@example.com',
+phone: '55 123 123', age: 23},
 ];
 
-const sumcart = cart.reduce((acc, curr) => acc + curr.price, 0);
+// const youngPeople = people
+//     .filter(person => person.age < 35)
+//     .map(person => ({ name: `${person.firstName} ${person.lastName}`, email: person.email }));
 
-console.log(sumcart);
+// console.log(youngPeople);
+
+// const numbers = [2, -30, 50, 20, -12, -9, 7];
+
+// const sum = numbers
+// .filter((number) => number > 0)
+// .reduce((sum, number) => sum + number, 0);
+
+// console.log(sum);
+
+
+const words = ['coder', 'developer', 'programmer'];
+
+const cWords = words.map((word) => {
+    return word[0].toUpperCase() + word.slice(1) + ' ' +  word.charAt(0).toUpperCase() + word.slice(1);
+});
+
+console.log(cWords);

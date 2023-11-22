f4c4861cb7348a106e6aa2cde783933953e13134
schugoshvili <schugoshvili4@gmail.com>
2023-11-22 20:33:06 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,79 +1,32 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// const doubledNumbers = numbers.map((number) => 'freakin ' + number);
+// const sum = numbers.reduce(function (accumulator, currentValue) {
+//     return accumulator + currentValue;
 
+// }, 0);
 
+const sum2 = numbers.reduce((acc, curr) => acc + curr, 0);
 
-// console.log(doubledNumbers); 
+console.log(sum2);
 
-// // Same with forEach
+// using a forLoop 
 
-// const doubledNumbers2 = [];
+const sum3 = () => {
+    let acc = 0;
+    for (const cur of numbers) {
+        acc += cur;
+    }
+    return acc;
+};
 
-// numbers.forEach((number) => {
-//     doubledNumbers2.push(number * 2);
-// });
+console.log(sum3());
 
-// console.log(doubledNumbers2);
+const cart = [
+    {id: 1, name: 'prod 1', price: 120},
+    {id: 2, name: 'prod 2', price: 556},
+    {id: 3, name: 'prod 3', price: 223},
+];
 
+const sumcart = cart.reduce((acc, curr) => acc + curr.price, 0);
 
-
-const companies = [
-    { name: 'BMW', category: 'Automotive', start: 1916, end: 2023 },
-    { name: 'Apple', category: 'Technology', start: 1976, end: 2023 },
-    { name: 'Nike', category: 'Apparel', start: 1964, end: 2023 },
-    { name: 'Microsoft', category: 'Technology', start: 1975, end: 2023 },
-    { name: 'Coca-Cola', category: 'Beverages', start: 1886, end: 2023 },
-    { name: 'Amazon', category: 'E-Commerce', start: 1994, end: 2023 },
-    { name: 'Google', category: 'Technology', start: 1998, end: 2023 },
-    { name: 'Samsung', category: 'Technology', start: 1938, end: 2023 },
-    { name: 'Toyota', category: 'Automotive', start: 1937, end: 2023 }
-  ];
-
-  // create an array of company names
-
-//   const companyNames = companies.map((company) => company.name
-//    );
-
-//    console.log(companyNames);
-
-//    // 
-
-//    const companyNC = companies.map((company) => company.name + ' - ' + company.category);
-
-//    console.log(companyNC);
-
-// const companyYears = companies.map((company) => {
-//     return {
-//         name: company.name,
-//         length: company.end - company.start
-//     }
-// }
-// );
-
-// console.log(companyYears);
-
-// const squareAndDouble = numbers
-// .map((number) => Math.sqrt(number))
-// .map((sqrt) => sqrt * 2);
-
-
-
-// const squareAndDouble2 = numbers
-// .map( function (number) {
-//     return Math.sqrt(number);
-// })
-// .map(function (sqrt) {
-//     return sqrt * 2;
-// })
-// .map(function (sqrtDoubled) {
-//     return sqrtDoubled * 2;
-// }
-// )
-
-
-const evenDouble = numbers
-.filter((number) => number % 2 === 0)
-.map((number) => number * 2);
-
-console.log(evenDouble);
+console.log(sumcart);

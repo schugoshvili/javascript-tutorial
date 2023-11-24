3b8b894e11fe26f1a79167fac83aa07d2c6fb1f2
schugoshvili <schugoshvili4@gmail.com>
2023-11-24 16:01:54 +0400

    Update custom1.js

--- shopping-list-main/custom1.js
+++ shopping-list-main/custom1.js
@@ -1,18 +1,48 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// capitalize challenge
 
-let newNum = numbers.reduce((numbers) => numbers % 2 === 0);
+let myString = 'developer';
 
-console.log(newNum);
+let newString = myString.charAt(0).toUpperCase() + myString.slice(1);
+
+//console.log(newString);
+
+// math challenge
+
+let x = Math.floor(Math.random() * 100 + 1);
+let y = Math.floor(Math.random() * 50 + 1);
+let sum = x + y;
+let difference = x - y;
+let product = x * y;
+let quotient = x / y;
+let remainder = x % y;
 
 
 
+// console.log(x, y)
+// console.log(sum);
+// console.log(difference);
+// console.log(product);
+// console.log(quotient);
+// console.log(remainder);
+
+// array challenge
+
+// challenge1 
+
+const arr = [1, 2, 3, 4, 5];
+
+arr.reverse();
+arr.push(0);
+arr.unshift(6);
+
+// console.log(arr);
 
 
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9, 10];
+
+const arr3 = [...arr1.slice(0, 4).concat(arr2)];
 
 
+console.log(arr3, typeof arr3);
 
-
-
-
-// Sum of even numbers (2 + 4 + 6 + 8 + 10)
-console.log(sumOfEvenNumbers(numbers)); // Output should be 30
